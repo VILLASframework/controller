3065227e2d2d68f39152ef84b3c4ddbdcb03d7d4
Steffen Vogel <post@steffenvogel.de>
2017-07-10 12:58:44 +0200

    automatically configure

--- config.py
+++ config.py
@@ -1,3 +1,8 @@
-# Connect to 137.226.248.91:5672 as villas with the password n0rw4y and virtual host "/" (%2F)
-#BROKER_URI = 'amqp://villas:n0rw4y@137.226.248.91/%2F?connection_attempts=3&heartbeat_interval=3600'
-BROKER_URI = 'amqp://villas:n0rw4y@137.226.248.91/%2F'
+import os
+
+if os.name == 'nt' and os.environ['USERDOMAIN'] == 'EONERC':
+	# RabbitMQ running on OpenStack
+	BROKER_URI = 'amqp://villas:n0rw4y@137.226.248.91/%2F?connection_attempts=3&heartbeat_interval=3600'
+else if 'BROKER_PORT' in os.environ:
+	# RabbitMQ running as docker container
+	BROKER_URI = 'amqp://villas:n0rw4y@broker/%2F'
