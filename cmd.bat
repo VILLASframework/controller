a0d1694ff9063d383d49becce8e2569b86c17391
Steffen Vogel <post@steffenvogel.de>
2018-01-30 15:14:48 +0100

    remove obsolete files

--- cmd.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-call C:\WinPython-64bit-2.7.9.1\scripts\env.bat
-
-cmd.exe /k
--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-if os.name == 'nt' and os.environ['USERDOMAIN'] == 'EONERC':
-	# RabbitMQ running on OpenStack
-	BROKER_URI = 'amqp://villas:n0rw4y@137.226.248.91/%2F?connection_attempts=3&heartbeat_interval=3600'
-else if 'BROKER_PORT' in os.environ:
-	# RabbitMQ running as docker container
-	BROKER_URI = 'amqp://villas:n0rw4y@broker/%2F'
