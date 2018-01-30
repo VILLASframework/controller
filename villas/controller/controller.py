3756b2560d183570937049cd4e3e2dd7842d345c
Steffen Vogel <post@steffenvogel.de>
2018-01-30 15:17:19 +0100

    reworked controller & discovery

---
+++ villas/controller/controller.py
@@ -0,0 +1,16 @@
+import logging
+import kombu.mixins
+
+LOGGER = logging.getLogger(__name__)
+
+class Controller(kombu.mixins.ConsumerMixin):
+
+	def __init__(self, connection, simulators):
+		self.simulators = simulators
+		self.connection = connection
+
+		for sim in simulators:
+			sim.set_connection(connection)
+
+	def get_consumers(self, Consumer, channel):
+		return map(lambda sim: sim.get_consumer(channel), self.simulators)
--- villas/controller/simulator.py
+++ villas/controller/simulator.py
@@ -1,20 +1,124 @@
+import logging
+import kombu
+import uuid
 
 class Simulator(object):
 
-	def __init__(self):
-		self.state = { 'state' : 'unknown' }
-	
-	def start(self, body):
-		print 'Starting simulation: ', body
-		
-	def stop(self, body):
-		print 'Stop simulation: ', body
+	def __init__(self, **args):
+		self.realm = args['realm']
+		self.type = args['type']
+		self.uuid = args['uuid'] or uuid.uuid4()
 
-	def pause(self, body):
-		print 'Pause simulation: ', body
+		self.properties = args
 
-	def resume(self, body):
-		print 'Resume simulation: ', body
-		
+		self.model = None
+		self._state = 'unknown'
+
+		self.logger = logging.getLogger(self.uuid)
+
+		self.exchange = kombu.Exchange(
+			name = 'villas',
+			type = 'headers',
+			durable = True
+		)
+
+	def set_connection(self, connection):
+		self.connection = connection
+
+		self.producer = kombu.Producer(
+			channel = self.connection.channel(),
+			exchange = self.exchange
+		)
+
+	@staticmethod
+	def from_json(json):
+		from .simulators import dummy, rtlab, rscad, dpsim
+
+		if json['type'] == "dummy":
+			return dummy.DummySimulator(**json)
+		elif json['type'] == "dpsim":
+			return dpsim.DPsimSimulator(**json)
+		elif json['type'] == "rtlab":
+			return dpsim.RTLabSimulator(**json)
+		elif json['type'] == "rscad":
+			return dpsim.RSCADSimulator(**json)
+		else:
+			return None
+
+	def get_consumer(self, channel):
+		self.channel = channel
+
+		return kombu.Consumer(
+			channel = self.channel,
+			on_message = self.on_message,
+			queues = kombu.Queue(
+				exchange = self.exchange,
+				binding_arguments = self.headers,
+				durable = False
+			),
+			no_ack = True
+		)
+
+	@property
+	def headers(self):
+		return {
+			'x-match' : 'any',
+			'category' : 'simulator',
+			'realm' : self.realm,
+			'uuid' : self.uuid
+		}
+
+	def on_message(self, message):
+		#self.logger.debug("Received message: %s: %s", message, message.payload)
+
+		if 'action' not in message.payload:
+			return
+
+		action = message.payload['action']
+
+		if action == 'ping':
+			self.ping(message)
+		elif action == 'start':
+			self.start(message)
+		elif action == 'stop':
+			self.stop(message)
+		elif action == 'pause':
+			self.pause(message)
+		elif action == 'resume':
+			self.resume(message)
+
+		message.ack()
+
+	@property
+	def state(self):
+		state = {
+			'state' : self._state,
+			'model' : self.model,
+			'properties' : self.properties
+		}
+
+		return state
+
+	def publish_state(self):
+		self.producer.publish(
+			self.state,
+			headers = self.headers
+		)
+
+	def ping(self, message):
+		self.publish_state()
+
+	def start(self, message):
+		pass
+
+	def stop(self, message):
+		pass
+
+	def pause(self, message):
+		pass
+
+	def resume(self, message):
+		pass
+
 	def __str__(self):
-		return self.name
+		return "Simulator <%s, %s>" % (self.type, self.uuid)
