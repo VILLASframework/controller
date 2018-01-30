7e47215ad458942084443ec6b218471ef171a344
Steffen Vogel <post@steffenvogel.de>
2018-01-30 15:17:31 +0100

    added dummy simulator for testing

---
+++ villas/controller/simulators/dummy.py
@@ -0,0 +1,85 @@
+import time
+import threading
+import os
+import socket
+
+import time
+
+from .. import simulator
+
+class DummySimulator(simulator.Simulator):
+
+	def __init__(self, **args):
+		args['type'] = 'dummy'
+
+		self.started = time.time()
+
+		super().__init__(**args)
+
+	@property
+	def headers(self):
+		headers = super().headers
+
+		headers['type'] = 'dummy'
+
+		return headers
+
+	@property
+	def state(self):
+		state = super().state
+
+		state['uptime'] = time.time() - self.started
+		state['version'] = '0.1.0'
+		state['host'] = socket.getfqdn()
+		state['kernel'] = os.uname()
+
+		return state
+
+	def change_state(self, state):
+		self.logger.info('Simulation %s' % state)
+		self._state = state
+		self.publish_state()
+
+	def start(self, message):
+		if self._state not in ['stopped', 'unkown'] :
+			self.logger.info('Starting simulation...')
+
+			t = threading.Timer(1.0, DummySimulator.change_state, args=[self, 'running'])
+			t.start()
+
+			self._state = 'starting'
+		else:
+			self.logger.warn('Attempted to start non-stopped simulator')
+
+	def stop(self, message):
+		if self._state == 'running':
+			self.logger.info('Stopping simulation...')
+
+			t = threading.Timer(1.0, DummySimulator.change_state, args=[self, 'stopped'])
+			t.start()
+
+			self._state = 'stopping'
+		else:
+			self.logger.warn('Attempted to stop non-running simulator')
+
+	def pause(self, message):
+		if self._state == 'running':
+			self.logger.info('Pausing simulation...')
+
+			t = threading.Timer(1.0, DummySimulator.change_state, args=[self, 'paused'])
+			t.start()
+
+			self._state = 'pausing'
+		else:
+			self.logger.warn('Attempted to pause non-running simulator')
+
+	def resume(self, message):
+		if self._state == 'paused':
+			self.logger.info('Resuming simulation...')
+
+			t = threading.Timer(1.0, DummySimulator.change_state, args=[self, 'running'])
+			t.start()
+
+			self._state = 'resuming'
+		else:
+			self.logger.warn('Attempted to resume non-paused simulator')
