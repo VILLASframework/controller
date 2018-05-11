ab7216b9172e39227b86edd6a692b2a9346a1a9d
Steffen Vogel <post@steffenvogel.de>
2018-05-11 14:12:50 +0200

    improve logging

--- villas/controller/simulators/dummy.py
+++ villas/controller/simulators/dummy.py
@@ -42,59 +42,51 @@ class DummySimulator(simulator.Simulator):
 
 	def start(self, message):
 		if self._state not in ['stopped', 'unkown'] :
-			self.logger.info('Starting simulation...')
-
 			t = threading.Timer(1.0, DummySimulator.change_state, args=[self, 'running'])
 			t.start()
 
 			self.change_state('starting')
 		else:
 			self.logger.warn('Attempted to start non-stopped simulator')
+			self.change_state('error')
 
 	def stop(self, message):
 		if self._state == 'running':
-			self.logger.info('Stopping simulation...')
-
 			t = threading.Timer(1.0, DummySimulator.change_state, args=[self, 'stopped'])
 			t.start()
 
 			self.change_state('stopping')
 		else:
 			self.logger.warn('Attempted to stop non-running simulator')
+			self.change_state('error')
 
 	def pause(self, message):
 		if self._state == 'running':
-			self.logger.info('Pausing simulation...')
-
 			t = threading.Timer(1.0, DummySimulator.change_state, args=[self, 'paused'])
 			t.start()
 
 			self.change_state('pausing')
 		else:
 			self.logger.warn('Attempted to pause non-running simulator')
+			self.change_state('error')
 
 	def resume(self, message):
 		if self._state == 'paused':
-			self.logger.info('Resuming simulation...')
-
 			t = threading.Timer(1.0, DummySimulator.change_state, args=[self, 'running'])
 			t.start()
 
 			self.change_state('resuming')
 		else:
 			self.logger.warn('Attempted to resume non-paused simulator')
+			self.change_state('error')
 
 	def shutdown(self, message):
-		self.logger.info('Shutdown simulator...')
-
 		t = threading.Timer(1.0, DummySimulator.change_state, args=[self, 'shutdown'])
 		t.start()
 
 		self.change_state('shuttingdown')
 
 	def reset(self, message):
-		self.logger.info('Resetting simulator...')
-
 		t = threading.Timer(1.0, DummySimulator.change_state, args=[self, 'idle'])
 		t.start()
 
