2d5b5ca599bb1b1c9ebc27adb47f97dfd19bf726
Steffen Vogel <post@steffenvogel.de>
2018-06-08 19:15:51 +0200

    improve setup.py:
    - get version from Git
    - set long_description from README.md
    - more fields

--- setup.py
+++ setup.py
@@ -1,22 +1,72 @@
 from setuptools import setup, find_packages
+from pkg_resources import parse_version  # part of `setuptools`
+from glob import glob
+import subprocess
+import re
+import os
+import git
+
+def git_version():
+	"""Return version with local version identifier."""
+
+	repo = git.Repo('.git')
+	repo.git.status()
+
+	latest_tag = repo.git.describe(match='v[0-9]*', tags=True, abbrev=0)
+	sha = repo.head.commit.hexsha[:6]
+
+	version = latest_tag.lstrip('v')
+
+	return '{v}-{sha}'.format(v = version, sha = sha)
+
+def cleanhtml(raw_html):
+	cleanr = re.compile('<.*?>')
+	cleantext = re.sub(cleanr, '', raw_html)
+	return cleantext
+
+def read(fname):
+	dname = os.path.dirname(__file__)
+	fname = os.path.join(dname, fname)
+
+	with open(fname) as f:
+		contents = f.read()
+		sanatized = cleanhtml(contents)
+
+	try:
+		from m2r import M2R
+		m2r = M2R()
+		return m2r(sanatized)
+	except:
+		return sanatized
 
 setup(
-	name="villas-controller",
-	version="0.1.0",
-	description="A controller/orchestration API for real-time power system simulators",
-	url="http://git.rwth-aachen.de/acs/public/villas/VILLAScontroller",
-	author="Steffen Vogel",
-	author_email="stvogel@eonrc.rwth-aachen.de",
-	classifiers=[  # Optional
+	name = 'villas-controller',
+	version = git_version(),
+	description = 'A controller/orchestration API for real-time power system simulators',
+	long_description = read('README.md'),
+	url = 'https://www.fein-aachen.org/projects/villas-controller/',
+	author = 'Steffen Vogel',
+	author_email = 'stvogel@eonrc.rwth-aachen.de',
+	maintainer = 'Steffen Vogel',
+	maintainer_email = 'stvogel@eonerc.rwth-aachen.de',
+	license = 'GPL-3.0',
+	keywords = 'simulation controller villas',
+	classifiers = [  # Optional
 		'Development Status :: 3 - Alpha',
 		'License :: OSI Approved :: GPL3',
 		'Programming Language :: Python :: 3'
 	],
-	packages=find_packages(),
-	install_requires=[
-		'kombu'
+	packages = find_packages(),
+	setup_requires = [
+        	'm2r',
+		'gitpython'
 	],
-	scripts=[
-		'bin/villas-ctl'
-	]
+	install_requires = [
+		'kombu',
+		'termcolor'
+	],
+	data_files = [
+		('/etc/villas/controller', glob('etc/*.json'))
+	],
+	scripts = glob('bin/*')
 )
