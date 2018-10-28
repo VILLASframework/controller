a81ea12df9af6a0606cca28042decc06a074591b
Steffen Vogel <post@steffenvogel.de>
2018-10-28 14:21:06 +0100

    version v0.3.0 and villas namespace package

--- setup.py
+++ setup.py
@@ -5,24 +5,6 @@ import subprocess
 import re
 import os
 
-def git_version():
-	"""Return version with local version identifier."""
-
-	try:
-		import git
-
-		repo = git.Repo('.git')
-		repo.git.status()
-
-		latest_tag = repo.git.describe(match='v[0-9]*', tags=True, abbrev=0)
-		sha = repo.head.commit.hexsha[:6]
-
-		version = latest_tag.lstrip('v')
-
-		return '{v}-{sha}'.format(v = version, sha = sha)
-	except:
-		return 'unknown'
-
 def cleanhtml(raw_html):
 	cleanr = re.compile('<.*?>')
 	cleantext = re.sub(cleanr, '', raw_html)
@@ -45,19 +27,17 @@ def read(fname):
 
 setup(
 	name = 'villas-controller',
-	version = git_version(),
+	version = '0.3.0',
 	description = 'A controller/orchestration API for real-time power system simulators',
 	long_description = read('README.md'),
 	url = 'https://www.fein-aachen.org/projects/villas-controller/',
 	author = 'Steffen Vogel',
-	author_email = 'stvogel@eonrc.rwth-aachen.de',
-	maintainer = 'Steffen Vogel',
-	maintainer_email = 'stvogel@eonerc.rwth-aachen.de',
+	author_email = 'acs-software@eonerc.rwth-aachen.de',
 	license = 'GPL-3.0',
 	keywords = 'simulation controller villas',
 	classifiers = [
 		'Development Status :: 3 - Alpha',
-		'License :: OSI Approved :: GPL3',
+		'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
 		'Programming Language :: Python :: 3'
 	],
 	packages = [ 'villas.controller' ],
