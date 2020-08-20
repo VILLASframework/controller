7f3486684f8ed89e247415021663a9c77562298e
Steffen Vogel <post@steffenvogel.de>
2020-08-20 15:31:52 +0200

    pep8 fixes

--- villas/controller/components/gateways/villas_relay.py
+++ villas/controller/components/gateways/villas_relay.py
@@ -1,123 +1,117 @@
 import threading
 import requests
-import time
 
 from ..gateway import Gateway
 
+
 class VILLASnodeRelaySession(Gateway):
 
-	def __init__(self, gateway, args):
-		# Some default properties
-		props = {
-			'category' : 'villas'
-		}
+    def __init__(self, gateway, args):
+        # Some default properties
+        props = {'category': 'villas'}
 
-		if args['type'] == 'websocket':
-			props['endpoint'] = gateway.endpoint + '/' + args['identifier']
+        if args['type'] == 'websocket':
+            props['endpoint'] = gateway.endpoint + '/' + args['identifier']
 
-		props.update(args)
+        props.update(args)
 
-		super().__init__(**props)
+        super().__init__(**props)
 
-		self.name = args['identifier']
-		self._state = args['state']
+        self.name = args['identifier']
+        self._state = args['state']
 
-		self.gateway = gateway
+        self.gateway = gateway
+
 
 class VILLASnodeRelay(Gateway):
 
-	def __init__(self, **args):
-		super().__init__(**args)
+    def __init__(self, **args):
+        super().__init__(**args)
 
-		self.sessions = []
-		self.endpoint = 'http://localhost:8088'
-		self.api_endpoint = self.endpoint + '/api/v1'
+        self.sessions = []
+        self.endpoint = 'http://localhost:8088'
+        self.api_endpoint = self.endpoint + '/api/v1'
 
-		if 'autostart' in args:
-			self.autostart = args['autostart']
-		else:
-			self.autostart = False
+        if 'autostart' in args:
+            self.autostart = args['autostart']
+        else:
+            self.autostart = False
 
-		self.version = 'unknown'
+        self.version = 'unknown'
 
-		self.thread_stop = threading.Event()
-		self.thread = threading.Thread(target = self.check_state_periodically)
-		self.thread.start()
+        self.thread_stop = threading.Event()
+        self.thread = threading.Thread(target=self.check_state_periodically)
+        self.thread.start()
 
-	def __del__(self):
-		self.thread_stop.set()
-		self.thread.join()
+    def __del__(self):
+        self.thread_stop.set()
+        self.thread.join()
 
-	def check_state_periodically(self):
-		while not self.thread_stop.wait(2):
-			self.check_state()
+    def check_state_periodically(self):
+        while not self.thread_stop.wait(2):
+            self.check_state()
 
-	def check_state(self):
-		try:
-			r = requests.get(self.api_endpoint)
-			r.raise_for_status()
+    def check_state(self):
+        try:
+            r = requests.get(self.api_endpoint)
+            r.raise_for_status()
 
-			for session in r.json['sessions']:
-				found = False
-				for comp in self.controller.components:
-					if comp.uuid == session['uuid']:
-						found = True
-						break
+            for session in r.json['sessions']:
+                found = False
+                for comp in self.controller.components:
+                    if comp.uuid == session['uuid']:
+                        found = True
+                        break
 
-				if found:
-					comp.change_state('running')
-				else:
-					session = VILLASnodeRelaySession(self, session)
+                if found:
+                    comp.change_state('running')
+                else:
+                    session = VILLASnodeRelaySession(self, session)
 
-					self.sessions.append(session)
-					self.controller.components.add(session)
+                    self.sessions.append(session)
+                    self.controller.components.add(session)
 
-			self.change_state('running')
+            self.change_state('running')
 
-		except requests.RequestException:
-			self.change_state('idle')
+        except requests.RequestException:
+            self.change_state('idle')
 
-	@property
-	def state(self):
-		return {
-			'villas_version' : self.version,
+    @property
+    def state(self):
+        return {
+            'villas_version': self.version,
+            **super().state
+        }
 
-			**super().state
-		}
+    def on_ready(self):
+        try:
+            self.version = self.node.get_version()
+        except Exception:
+            self.change_state('error', error='VILLASnode not installed')
 
-	def on_ready(self):
-		try:
-			self.version = self.node.get_version()
+        if self.autostart:
+            self.start()
 
-		except:
-			self.change_state('error', error='VILLASnode not installed')
+    def start(self, message=None):
 
-		if self.autostart:
-			self.start()
+        self.change_state('starting')
 
-	def start(self, message = None):
+    def stop(self, message):
 
+        self.change_state('idle')
 
-		self.change_state('starting')
+        # Once the gateway shutsdown, all the gateway nodes are also shutdown
+        for session in self.sessions:
+            session.change_state('shutdown')
 
-	def stop(self, message):
+    def pause(self, message):
 
+        # Once the gateway is paused, all the gateway nodes are also paused
+        for session in self.sessions:
+            session.change_state('paused')
 
-		self.change_state('idle')
+    def resume(self, message):
+        pass
 
-		# Once the gateway shutsdown, all the gateway nodes are also shutdown
-		for session in self.sessions:
-			session.change_state('shutdown')
-
-	def pause(self, message):
-
-
-		# Once the gateway is paused, all the gateway nodes are also paused
-		for session in self.sessions:
-			session.change_state('paused')
-
-	def resume(self, message):
-		pass
-
-	def reset(self, message):
-		pass
+    def reset(self, message):
+        pass
