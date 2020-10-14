7f6abdef40c0ca36ef3a25aad9c87d6bbd8ee149
Steffen Vogel <post@steffenvogel.de>
2020-10-14 13:05:39 +0200

    add some example Dockerfile for a k8s job

---
+++ tests/kubernetes/entrypoint.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+sleep_interruptible() {
+	REM=$1
+
+	while (( ${REM} > 0 )); do
+		sleep 1
+		REM=$((${REM} - 1))
+		echo "Remaining: ${REM}"
+	done
+}
+
+trap_with_arg() {
+	func="$1" ; shift
+	for sig ; do
+		trap "$func $sig" "$sig"
+	done
+}
+
+trap_handler() {
+	echo "Received signal $1"
+
+	if [[ $1 == "TERM" ]]; then
+		echo "Now terminating.."
+		exit
+	fi
+}
+
+trap_with_arg trap_handler INT TERM CONT EXIT
+
+RUNTIME=${RUNTIME:-120}
+
+echo "My PID is $$"
+echo "Starting workload for ${RUNTIME} seconds"
+
+sleep_interruptible ${RUNTIME}
