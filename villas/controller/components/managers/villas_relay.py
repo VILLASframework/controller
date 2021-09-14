5f334894f36dcc92571c15f8a3e0345425127be2
Iris Marie Köster <ikoester@eonerc.rwth-aachen.de>
2021-09-14 19:57:19 +0200

    Relay node properties

--- villas/controller/components/managers/villas_relay.py
+++ villas/controller/components/managers/villas_relay.py
@@ -1,5 +1,6 @@
 import threading
 import requests
+import os
 
 from villas.controller.components.manager import Manager
 from villas.controller.components.gateways.villas_relay import VILLASrelayGateway  # noqa E501
@@ -9,87 +10,108 @@ class VILLASrelayManager(Manager):
 
     def __init__(self, **args):
         self.autostart = args.get('autostart', False)
-        self.ws_url = args.get('api_url', 'http://localhost:8088')
         self.api_url = args.get('api_url', 'http://localhost:8088') + '/api/v1'
+        self.api_url_external = args.get('api_url_external', self.api_url)
+        self._version = '-1'
 
-        self.status = self.get_status()
-        self.version = self.status['version']
-
-        args['uuid'] = self.status['uuid']
+        uuid = self.get_uuid()
+        if uuid is not None:
+            args['uuid'] = uuid
 
         super().__init__(**args)
+
+        self.properties['api_url'] = self.api_url_external
 
         self.thread_stop = threading.Event()
         self.thread = threading.Thread(target=self.reconcile_periodically)
         self.thread.start()
 
-    def __del__(self):
-        self.thread_stop.set()
-        self.thread.join()
-
     def reconcile_periodically(self):
         while not self.thread_stop.wait(2):
             self.reconcile()
 
-    def get_status(self):
-        r = requests.get(self.api_url)
-        r.raise_for_status()
-
-        return r.json()
-
-    def reconcile(self):
+    def get_uuid(self):
         try:
-            self.status = self.get_status()
+            r = requests.get(self.api_url)
+            r.raise_for_status()
+            return r.json().get('uuid')
+        except requests.exceptions.RequestException:
+            return None
 
-            active_sessions = self.status['sessions']
+    def get_status(self):
+        try:
+            r = requests.get(self.api_url)
+            r.raise_for_status()
 
-            active_uuids = {session['uuid'] for session in active_sessions}
-            existing_uuids = set(self.components.keys())
+            return r.json()
 
-            # Add new sessions and update existing ones
-            for session in active_sessions:
-                uuid = session['uuid']
-
-                if uuid in self.components:
-                    ic = self.components[uuid]
-
-                    ic.change_state('running')
-                else:
-                    ic = VILLASrelayGateway(self, session)
-
-                    self.add_component(ic)
-
-            # Find vanished sessions
-            for uuid in existing_uuids - active_uuids:
-                ic = self.components[uuid]
-
-                self.remove_component(ic)
-
-            if len(active_sessions) > 0:
-                self.change_state('running')
-            else:
-                self.change_state('paused')
-
-        except requests.RequestException:
+        except requests.exceptions.RequestException:
             self.change_state('error', error='Failed to contact VILLASrelay')
 
+            return None
+
+    def reconcile(self):
+        status = self.get_status()
+        if status is None:
+            return
+
+        if self._state == 'error':
+            self.change_state('idle')
+
+        self._status = status
+        self._version = self._status['version']
+
+        active_sessions = self._status['sessions']
+        active_uuids = {session['uuid'] for session in active_sessions}
+        existing_uuids = set(self.components.keys())
+
+        # Add new sessions and update existing ones
+        for session in active_sessions:
+            uuid = session['uuid']
+
+            if uuid in self.components:
+                comp = self.components[uuid]
+
+                comp.change_state('running')
+            else:
+                comp = VILLASrelayGateway(self, session)
+
+                self.add_component(comp)
+
+        # Find vanished sessions
+        for uuid in existing_uuids - active_uuids:
+            comp = self.components[uuid]
+
+            self.remove_component(comp)
+
+        if len(self.components) > 0:
+            self.change_state('running')
+        else:
+            self.change_state('paused')
+
     @property
-    def state(self):
+    def status(self):
         return {
-            'villas_relay_version': self.version,
-            **super().state
+            'villas_relay_version': self._version,
+            **super().status
         }
 
-    def on_ready(self):
-        try:
-            r = self._api_request()
+    def on_shutdown(self):
+        self.thread_stop.set()
+        self.thread.join()
 
-            self.version = r['version']
+        return super().on_shutdown()
+
+    def on_ready(self):
+        if self.autostart:
+            os.system('villas-relay')
+
+        try:
+            status = self.get_status()
+
+            self._version = status['version']
 
         except Exception:
             self.change_state('error', error='Failed to contact VILLASrelay')
-
-        # if self.autostart:
-        #   os.system('villas-relay')
 
         super().on_ready()
