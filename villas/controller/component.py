21af994c5071a49930a83935c9889b28a89f8d68
Iris Marie Köster <ikoester@eonerc.rwth-aachen.de>
2022-01-12 10:09:05 +0000

    fix formatting
    Signed-off-by: Iris Koester <iris.m.koester@gmail.com>

--- villas/controller/component.py
+++ villas/controller/component.py
@@ -291,8 +291,9 @@ class Component:
         self.mixin.publish(self.status, headers=self.headers)
 
     def publish_status_periodically(self):
-        self.logger.info('Start state publish thread, initial status: %s', self.status)
-        self.publish_status() # publish the first update immediately
+        self.logger.info('Start state publish thread, initial status: %s',
+                         self.status)
+        self.publish_status()  # publish the first update immediately
 
         while not self.publish_status_thread_stop.wait(
           self.publish_status_interval):
