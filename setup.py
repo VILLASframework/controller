25706270a2b98b5eee3bdc5f613aacf80fd4a6c0
Steffen Vogel <steffen.vogel@opal-rt.com>
2023-05-02 13:11:29 +0200

    fix version number if package is not installed
    Signed-off-by: Steffen Vogel <steffen.vogel@opal-rt.com>

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup, find_namespace_packages
 from glob import glob
 
+from villas.controller import __version__ as version
+
 with open('README.md') as f:
     long_description = f.read()
 
 setup(
     name='villas-controller',
-    version='0.3.2',
+    version=version,
     description='A controller/orchestration API for real-time '
                 'power system simulators',
     long_description=long_description,
